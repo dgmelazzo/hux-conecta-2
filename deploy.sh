577ff66ea3512c692c6aee9096207f9a41973d6b
CTO HUX <cto@hux.com.br>
2026-04-14 13:50:19 +0000

    chore(GRU-99): atualiza deploy.sh para hux-conecta-2

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,4 @@
 #!/bin/bash
-# deploy.sh  Conecta 2.0 (conecta.acicdf.org.br)
-# Uso: bash deploy.sh
-set -e
-REPO="/var/www/conecta"
-BRANCH="main"
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Iniciando deploy Conecta 2.0..."
-cd "$REPO"
-git fetch origin
-git reset --hard origin/$BRANCH
-nginx -t && systemctl reload nginx
-echo "[$(date '+%Y-%m-%d %H:%M:%S')] Deploy Conecta 2.0 concluido. Branch: $BRANCH"
+cd /var/www/conecta
+git pull origin main
+echo "Deploy Conecta 2.0 concluido"
